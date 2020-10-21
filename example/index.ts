4b57d0e7a7171b897be7da5e36b6eecae2049d25
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-21 22:00:33 +0200

    add thread spawner

--- example/index.ts
+++ example/index.ts
@@ -1,1 +1,5 @@
-console.log('hello');
+import { Scheduler } from '../src';
+
+const scheduler = new Scheduler(1024);
+
+console.log(scheduler);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 // Internal
-import { Thread } from './internal';
-
-console.log('hello');
+import { Thread } from './internal/Thread';
 
 // SharedArrayBuffer
 // Semaphore
@@ -13,10 +11,28 @@ console.log('hello');
 
 // lock() / release() like Mutex locks
 
-export class Scheduler {
-  private size: number;
+const align32Bits = (v: number) => (v & 0xffffffffffffc) + (v & 0x3 ? 0x4 : 0);
 
-  constructor(size: number) {
-    this.size = size || Math.min(Math.max(navigator.hardwareConcurrency, 2), 4);
+export class Scheduler {
+  private sharedBuffer: SharedArrayBuffer;
+  private sharedBufferArray: Uint32Array;
+  private threadCount: number;
+  private threads: Thread[];
+
+  constructor(bufferSize: number, threadCount?: number) {
+    if (!(bufferSize > 0)) {
+      throw new RangeError('bufferSize must be a positive number');
+    }
+
+    // Align to 32-bits
+    bufferSize = align32Bits(bufferSize);
+
+    this.threadCount =
+      threadCount || Math.min(Math.max(navigator.hardwareConcurrency, 2), 4);
+    this.sharedBuffer = new SharedArrayBuffer(
+      Uint32Array.BYTES_PER_ELEMENT * bufferSize
+    );
+    this.sharedBufferArray = new Uint32Array(this.sharedBuffer);
+    this.threads = [...Array(this.threadCount)].map(() => new Thread());
   }
 }
