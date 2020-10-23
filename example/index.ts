f64f86073cae38a6975ffb193acf9950816ad0db
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2020-10-23 21:27:16 +0200

    remove dependency on @thi.ng/heaps

--- example/index.ts
+++ example/index.ts
@@ -1,33 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-// import { Priorities, Scheduler } from '../src';
+import { Priorities, Scheduler } from '../src';
 
-// const scheduler = new Scheduler();
+const scheduler = new Scheduler();
 
-// const getUser = async (username: string) => {
-//   const url = `https://api.github.com/users/${username}`;
-//   const res = await fetch(url);
-//   const profile = await res.json();
-//   return profile.name;
-// };
+const getUser = async (username: string) => {
+  const url = `https://api.github.com/users/${username}`;
+  const res = await fetch(url);
+  const profile = await res.json();
+  return profile.name;
+};
 
-// (async () => {
-//   // await Promise.all([
-//   //   scheduler.addTask(Priorities.LowPriority, getUser, ['developit']),
-//   //   scheduler.addTask(Priorities.ImmediatePriority, getUser, [
-//   //     'timvanscherpenzeel',
-//   //   ]),
-//   // ]).then((response: any) => {
-//   //   console.log(response);
-//   // });
+(async () => {
+  await Promise.all([
+    scheduler.addTask(Priorities.LowPriority, getUser, ['developit']),
+    scheduler.addTask(Priorities.ImmediatePriority, getUser, [
+      'timvanscherpenzeel',
+    ]),
+  ]).then((response: any) => {
+    console.log(response);
+  });
 
-//   // const a = scheduler.addTask(Priorities.LowPriority, getUser, ['developit']);
-//   const b = scheduler.addTask(Priorities.ImmediatePriority, getUser, [
-//     'timvanscherpenzeel',
-//   ]);
+  // // const a = scheduler.addTask(Priorities.LowPriority, getUser, ['developit']);
+  // const b = scheduler.addTask(Priorities.ImmediatePriority, getUser, [
+  //   'timvanscherpenzeel',
+  // ]);
 
-//   // const d = await a;
-//   const e = await b;
+  // // const d = await a;
+  // const e = await b;
 
-//   console.log(e);
-// })();
+  // console.log(e);
+})();
+
+// import { BinaryHeap } from '../src/internal/BinaryHeap';
+
+// const h = new BinaryHeap();
+// h.push({ priority: 5, taskId: 2 });
+// h.push({ priority: 1, taskId: 0 });
+// h.push({ priority: 2, taskId: 1 });
+// h.push({ priority: 3, taskId: 3 });
+
+// console.log(h.pop()); // 5
+// console.log(h.pop()); // 10
+// console.log(h.pop()); // 15
+// console.log(h.pop()); // 20
+// console.log(h.pop()); // undefined
--- src/internal/PriorityQueue.ts
+++ src/internal/PriorityQueue.ts
@@ -1,54 +1,29 @@
-// Vendor
-import { Heap } from '@thi.ng/heaps';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
-interface Entry {
-  priority: number;
-  data: unknown[];
-}
+// Internal
+import { BinaryHeap } from './BinaryHeap';
 
 /**
  * A priority queue implementation based on https://github.com/thi-ng/umbrella/blob/develop/packages/heaps/src/heap.ts
  */
 export class PriorityQueue {
   private map: Map<number, unknown[]> = new Map();
-  private heap: Heap<number> = new Heap<number>();
-
-  constructor(entries?: Entry[]) {
-    if (entries) {
-      this.into(entries);
-    }
-  }
+  private heap: BinaryHeap = new BinaryHeap();
 
   get length() {
     return this.heap.length;
   }
 
-  public into(entries: Entry[]) {
-    entries.forEach(({ priority, data }) => this.push(priority, data));
-  }
-
-  public push(priority: number, data: unknown[]) {
-    this.map.set(priority, data);
-    this.heap.push(priority);
+  public push(priority: number, taskId: number, data: unknown[]) {
+    this.map.set(taskId, data);
+    this.heap.push({ priority, taskId });
   }
 
   public pop() {
-    const key = this.heap.pop();
-    const value = this.map.get(key);
-    this.map.delete(key);
+    const { taskId } = this.heap.pop();
+    const data = this.map.get(taskId);
+    this.map.delete(taskId);
 
-    return value;
-  }
-
-  public peek() {
-    const key = this.heap.peek();
-    const value = this.map.get(key);
-
-    return value;
-  }
-
-  public clear() {
-    this.heap.clear();
-    this.map.clear();
+    return data;
   }
 }
